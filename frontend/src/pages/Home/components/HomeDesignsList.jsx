f17391b55c75249cc9559a7c86e53857d790316c
Damisa Alex <damisa.alex@gmail.com>
2023-08-12 18:51:46 +0100

    updated frontend

--- frontend/src/pages/Home/components/HomeDesignsList.jsx
+++ frontend/src/pages/Home/components/HomeDesignsList.jsx
@@ -1,143 +1,139 @@
-import finimedia from './images/finimedia.jpg';
-import finimedia2 from './images/finimedia2.jpg';
-import academy from './images/academy.jpg';
-import alternatio from './images/alternatio.jpg';
-import alternatio2 from './images/alternatio2.jpg';
-import baby from './images/baby.jpg';
-import educore from './images/educore.jpg';
-import everything from './images/everything.jpg';
-import heywrita from './images/heywrita.jpg';
-import heywrita2 from './images/heywrita2.jpg';
-import izyfood from './images/izyfood.jpg';
-import merit from './images/merit.jpg';
-import pawfinder from './images/pawfinder.jpg';
-import shopcademy from './images/shopcademy.jpg';
-import chat from './images/chat.jpg';
-import roster from './images/roster.jpg';
-import alarmeet from './images/alarmeet.jpg';
-import edfog from './images/edfog.jpg';
+import { Link } from "react-router-dom";
 
-import classes from './HomeDesignsList.module.css';
+import classes from "./HomeDesignsList.module.css";
 
 const HomeDesignList = () => {
   return (
     <>
       <div className={classes.container}>
         <div className={classes.designitem}>
-          <img src={finimedia} alt="finimedia" />
-          <h2>Finimedia</h2>
+          <Link to="https://dazzleberrydesigns.com/work-item/64d511da67b90ea3073db5c9">
+            <img src="/images/home/1.jpg" alt="pawfinder" />
+            <h2>Pawfinder</h2>
+          </Link>
         </div>
         <div className={classes.designitem}>
-          <img src={pawfinder} alt="pawfinder" />
-          <h2>Pawfinder</h2>
+          <Link to="https://dazzleberrydesigns.com/work-item/64d50fe267b90ea3073db5ba">
+            <img src="/images/home/2.jpeg" alt="landvault" />
+            <h2>Landvault</h2>
+          </Link>
         </div>
         <div className={classes.designitem}>
-          <img src={alternatio} alt="alternatio" />
-          <h2>Alternatio Navitas</h2>
+          <Link to="https://dazzleberrydesigns.com/work-item/64d4d08267b90ea3073db5b3">
+            <img src="/images/home/3.jpg" alt="finimedia" />
+            <h2>Finimedia</h2>
+          </Link>
         </div>
         <div className={classes.designitem}>
-          <img src={baby} alt="baby" />
-          <h2>Baby Republic</h2>
+          <Link to="https://dazzleberrydesigns.com/work-item/64d4bd2467b90ea3073db5ac">
+            <img src="/images/home/4.jpg" alt="myAsoebi" />
+            <h2>MyAsoebi</h2>
+          </Link>
         </div>
         <div className={classes.designitem}>
-          <img src={everything} alt="everything" />
-          <h2>Everything Beautiful</h2>
+          <Link to="https://dazzleberrydesigns.com/work-item/64cea60767b90ea3073db4d2">
+            <img src="/images/home/5.jpeg" alt="alarmeet" />
+            <h2>Alarmeet</h2>
+          </Link>
         </div>
         <div className={classes.designitem}>
-          <img src={merit} alt="merit" />
-          <h2>MeritFX</h2>
+          <Link to="https://dazzleberrydesigns.com/work-item/64ce2f7667b90ea3073db4a4">
+            <img src="/images/home/6.jpeg" alt="Ebenezer Victoria Foundation" />
+            <h2>Ebenezer Victoria Foundation</h2>
+          </Link>
         </div>
         <div className={classes.designitem}>
-          <img src={heywrita} alt="heywrita" />
-          <h2>Heywrita</h2>
+          <Link to="https://dazzleberrydesigns.com/work-item/64cd56d367b90ea3073db459">
+            <img src="/images/home/7.jpeg" alt="heyWrita" />
+            <h2>HeyWrita</h2>
+          </Link>
         </div>
         <div className={classes.designitem}>
-          <img src={shopcademy} alt="shopcademy" />
-          <h2>Shopcademy</h2>
+          <Link to="https://dazzleberrydesigns.com/work-item/64cd470867b90ea3073db450">
+            <img src="/images/home/8.jpeg" alt="aihoun" />
+            <h2>Aihoun</h2>
+          </Link>
         </div>
         <div className={classes.designitem}>
-          <img src={izyfood} alt="izyfood" />
-          <h2>Izy Food Hub</h2>
+          <Link to="https://dazzleberrydesigns.com/work-item/64cd382467b90ea3073db447">
+            <img src="/images/home/9.jpeg" alt="Orinda Fresh" />
+            <h2>Orinda Fresh</h2>
+          </Link>
         </div>
         <div className={classes.designitem}>
-          <img src={academy} alt="academy" />
-          <h2>Academy Stack</h2>
-        </div>
-        <div className={classes.designitem}>
-          <img src={educore} alt="educore" />
-          <h2>Educore</h2>
-        </div>
-        <div className={classes.designitem}>
-          <img src={chat} alt="chat" />
-          <h2>The Chat Box</h2>
+          <Link to="https://dazzleberrydesigns.com/work-item/64cd22d867b90ea3073db410">
+            <img src="/images/home/10.jpg" alt="2121 The Brand" />
+            <h2>2121 The Brand</h2>
+          </Link>
         </div>
       </div>
       <div className={classes.container_d}>
         <div className={classes.designitem_d}>
-          <img src={finimedia2} alt="finimedia" />
-          <h2>Finimedia</h2>
-        </div>
-        <div className={classes.designitem_d}>
-          <div className={classes.designitem_d_c}>
-            <img src={pawfinder} alt="pawfinder" />
+          <Link to="https://dazzleberrydesigns.com/work-item/64d511da67b90ea3073db5c9">
+            <img src="/images/home/1.jpg" alt="pawfinder" />
             <h2>Pawfinder</h2>
+          </Link>
+        </div>
+        <div className={classes.designitem_d}>
+          <div className={classes.designitem_d_c}>
+            <Link to="https://dazzleberrydesigns.com/work-item/64d50fe267b90ea3073db5ba">
+              <img src="/images/home/2.jpeg" alt="landvault" />
+              <h2>Landvault</h2>
+            </Link>
           </div>
           <div className={classes.designitem_d_c}>
-            <img src={baby} alt="baby" />
-            <h2>Baby Republic</h2>
+            <Link to="https://dazzleberrydesigns.com/work-item/64d4bd2467b90ea3073db5ac">
+              <img src="/images/home/4.jpg" alt="myAsoebi" />
+              <h2>MyAsoebi</h2>
+            </Link>
           </div>
           <div className={classes.designitem_d_c}>
-            <img src={merit} alt="merit" />
-            <h2>MeritFX</h2>
+            <Link to="https://dazzleberrydesigns.com/work-item/64ce2f7667b90ea3073db4a4">
+              <img
+                src="/images/home/6.jpeg"
+                alt="Ebenezer Victoria Foundation"
+              />
+              <h2>Ebenezer Victoria Foundation</h2>
+            </Link>
           </div>
           <div className={classes.designitem_d_c}>
-            <img src={everything} alt="everything" />
-            <h2>Everything Beautiful</h2>
+            <Link to="https://dazzleberrydesigns.com/work-item/64cea60767b90ea3073db4d2">
+              <img src="/images/home/5.jpeg" alt="alarmeet" />
+              <h2>Alarmeet</h2>
+            </Link>
           </div>
         </div>
         <div className={classes.designitem_d}>
           <div className={classes.designitem_d_c}>
-            <img src={roster} alt="roster" />
-            <h2>Roster</h2>
+            <Link to="https://dazzleberrydesigns.com/work-item/64cd56d367b90ea3073db459">
+              <img src="/images/home/7.jpeg" alt="heyWrita" />
+              <h2>HeyWrita</h2>
+            </Link>
           </div>
           <div className={classes.designitem_d_c}>
-            <img src={shopcademy} alt="shopcademy" />
-            <h2>Shopcademy</h2>
+            <Link to="https://dazzleberrydesigns.com/work-item/64cd470867b90ea3073db450">
+              <img src="/images/home/8.jpeg" alt="aihoun" />
+              <h2>Aihoun</h2>
+            </Link>
           </div>
           <div className={classes.designitem_d_c}>
-            <img src={izyfood} alt="izyfood" />
-            <h2>Izy Food Hub</h2>
+            <Link to="https://dazzleberrydesigns.com/work-item/64cd382467b90ea3073db447">
+              <img src="/images/home/9.jpeg" alt="Orinda Fresh" />
+              <h2>Orinda Fresh</h2>
+            </Link>
           </div>
           <div className={classes.designitem_d_c}>
-            <img src={educore} alt="educore" />
-            <h2>Educore</h2>
+            <Link to="https://dazzleberrydesigns.com/work-item/64cd22d867b90ea3073db410">
+              <img src="/images/home/10.jpg" alt="2121 The Brand" />
+              <h2>2121 The Brand</h2>
+            </Link>
           </div>
         </div>
         <div className={classes.designitem_d}>
-          <img src={alternatio2} alt="alternatio" />
-          <h2>Alternatio Navitas</h2>
-        </div>
-        <div className={classes.designitem_d}>
-          <img src={heywrita2} alt="heywrita" />
-          <h2>Heywrita</h2>
-        </div>
-        <div className={classes.designitem_d}>
-          <div className={classes.designitem_d_c}>
-            <img src={academy} alt="academy" />
-            <h2>Academy Stack</h2>
-          </div>
-          <div className={classes.designitem_d_c}>
-            <img src={alarmeet} alt="alarmeet" />
-            <h2>Alarmeet</h2>
-          </div>
-          <div className={classes.designitem_d_c}>
-            <img src={chat} alt="chat" />
-            <h2>The Chat Box</h2>
-          </div>
-          <div className={classes.designitem_d_c}>
-            <img src={edfog} alt="edfog" />
-            <h2>EDFOG</h2>
-          </div>
+          <Link to="https://dazzleberrydesigns.com/work-item/64d4d08267b90ea3073db5b3">
+            <img src="/images/home/3.jpg" alt="finimedia" />
+            <h2>Finimedia</h2>
+          </Link>
         </div>
       </div>
     </>
